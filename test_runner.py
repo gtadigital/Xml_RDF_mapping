7411df654356409ae359596e184f3516254a73c9
matteoLorenzini <matteo-lorenzini@outlook.it>
2021-08-03 14:00:16 +0200

    Added .gitgnore and test_runner

---
+++ test_runner.py
@@ -0,0 +1,5 @@
+import xml_to_rdf_mapper_final_with_arguments
+from xml_to_rdf_mapper_final_with_arguments import transform
+
+xml_to_rdf_mapper_final_with_arguments.transform("C:\\Users\\Zoe\\Documents\\Arbeit gta\\x3ml rdf transformation\\Place\\input_xml_place.xml", "C:\\Users\\Zoe\\Documents\\Arbeit gta\\x3ml rdf transformation\\Place\\x3mlMapping_place.xml", "C:\\Users\\Zoe\\Documents\\Arbeit gta\\x3ml rdf transformation\\Place\\generator-policy_place.xml", "C:\\Users\\Zoe\\Documents\\Arbeit gta\\x3ml rdf transformation\\Place\\output_place_new.rdf", "./entry/plIdentifier_uuid")
+print("done")
