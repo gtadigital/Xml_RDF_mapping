39025c2db7849235c0a41b168194d1edc56273aa
ZCRNK <36694085+ZCRNK@users.noreply.github.com>
2021-10-01 18:26:01 +0200

    Update runner.py
    added arguments to runner.py

--- runner.py
+++ runner.py
@@ -1,5 +1,22 @@
-import xml_to_rdf_mapper
-from xml_to_rdf_mapper import transformAll
+import argparse
 
-xml_to_rdf_mapper.transformAll("..\\xml_source.xml", "..x3ml_mapping.xml", "..\\generator-policy.xml", "..\\output_BW_new.rdf", "./entry/oeu_nc_uuid")
-print("done")
+import xml_to_rdf_mapper_final_with_arguments
+from xml_to_rdf_mapper_final_with_arguments import transform
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='run xml to rdf transformation')
+    parser.add_argument('--source_XML', dest="source_XML",help='path of the source XML file')
+    parser.add_argument('--gen_pol', dest="gen_pol", help='path of the generator policy file')
+    parser.add_argument('--x3ml_mapping', dest="x3ml_mapping", help='path of the X3ML mapping file')
+    parser.add_argument('--out_path', dest="out_path", help='path of the folder which will contain the output files' )
+    parser.add_argument('--path_to_uuid', dest="path_to_uuid", help='path to the uuid tag in the source XML file')
+
+    args = parser.parse_args()
+    source_XML = args.source_XML
+    gen_pol = args.gen_pol
+    x3ml_mapping = args.x3ml_mapping
+    out_path = args.out_path
+    path_to_uuid = args.path_to_uuid
+
+    xml_to_rdf_mapper_final_with_arguments.transformAll(source_XML, x3ml_mapping, gen_pol, out_path, path_to_uuid)
+    print("done")
