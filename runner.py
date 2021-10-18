6de894074335c0fcd64f14cee2c13a6b77d1e387
matteoLorenzini <matteo-lorenzini@outlook.it>
2021-10-18 17:00:26 +0200

    Fixed issue #16 and issue #21

--- runner.py
+++ runner.py
@@ -1,11 +1,10 @@
 import argparse
-
+import os
 import xml_to_rdf_mapper
-from xml_to_rdf_mapper import transform
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='run xml to rdf transformation')
-    parser.add_argument('--source_XML', dest="source_XML",help='path of the source XML file')
+    parser.add_argument('--source_XML', dest="source_XML",help='path of the folder containing the source XML files')
     parser.add_argument('--gen_pol', dest="gen_pol", help='path of the generator policy file')
     parser.add_argument('--x3ml_mapping', dest="x3ml_mapping", help='path of the X3ML mapping file')
     parser.add_argument('--out_path', dest="out_path", help='path of the folder which will contain the output files' )
@@ -18,5 +17,14 @@ if __name__ == "__main__":
     out_path = args.out_path
     path_to_uuid = args.path_to_uuid
 
-    xml_to_rdf_mapper.transformAll(source_XML, x3ml_mapping, gen_pol, out_path, path_to_uuid)
+    counter = 0
+    for root, dirs, files in os.walk(source_XML):
+
+        for item in files:
+            if item.endswith(('.xml')):
+                whole_input_path = root+"/"+ item
+                modified_output_path= out_path+"/file"+str(counter)+"_entry.rdf"
+                xml_to_rdf_mapper.transformAll(whole_input_path, x3ml_mapping, gen_pol, modified_output_path, path_to_uuid)
+                counter+=1
+    
     print("done")
